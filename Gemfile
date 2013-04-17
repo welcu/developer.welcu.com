0244981f40f598ec7bd69fed30fc307e35700d77
Seba Gamboa <me@sagmor.com>
2013-04-17 14:48:07 -0300

    Unfinnished version of the developer documentation

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem "middleman", "~>3.0.13"
+# gem "middleman-syntax"
+gem "middleman-deploy"
+gem 'redcarpet'
+gem "bootstrap-sass", "~> 2.3.1.0", git: 'git://github.com/thomas-mcdonald/bootstrap-sass.git'
+gem "coderay"
---
+++ source/javascripts/application.js
@@ -0,0 +1,13 @@
+//= require jquery
+//= require bootstrap
+//= require_tree .
+
+// side bar
+// setTimeout(function () {
+//   $('.navigation').children().first().affix({
+//     offset: {
+//       top: function () { return $(window).width() <= 980 ? 290 : 210 }
+//     , bottom: 270
+//     }
+//   })
+// }, 100);
